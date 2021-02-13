75e4502d86a354b003c653cec4ccc8ef371881f4
thyezoliveira <thyezoliveira@gmail.com>
2021-02-13 20:09:36 -0300

    JsWeb.fim
    Terminei o curso de Javascript web.

---
+++ JsWeb/introducao-javascript/js/calcula-imc.js
@@ -0,0 +1,54 @@
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista";
+
+var pacientes = document.querySelectorAll(".paciente");
+
+for(var i = 0; i < pacientes.length; i++){
+    var paciente = pacientes[i];
+    var tdPeso = paciente.querySelector(".info-peso");
+    var tdAltura = paciente.querySelector(".info-altura");
+    var tdImc = paciente.querySelector(".info-imc");
+    var peso = tdPeso.textContent;
+    var altura = tdAltura.textContent;
+    var pesoEhValido = validaPeso(peso);
+    var alturaEhValida = validaAltura(altura);
+    
+    if(!pesoEhValido){
+        pesoEhValido = false;
+        tdImc.textContent = "Peso inválido";
+        paciente.classList.add("paciente-invalido");
+    }
+    
+    if(!alturaEhValida){
+        alturaEhValida = false;
+        tdImc.textContent = "Altura inválida";
+        paciente.classList.add("paciente-invalido");
+    }
+    
+    if(pesoEhValido && alturaEhValida){
+        var imc = calculaImc(peso, altura);
+        tdImc.textContent = imc;
+    }
+}
+
+function validaPeso(peso){
+    if(peso >= 0 && peso < 1000){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function validaAltura(altura){
+    if(altura >= 0 && altura <= 3.00){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function calculaImc(peso, altura){
+    var imc = 0;
+    imc = peso / (altura * altura);
+    return imc.toFixed(2);
+}
