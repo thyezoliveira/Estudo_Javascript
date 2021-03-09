3f640f2ff7e6310b26bb3c25f25d99dab1b97b3c
thyezoliveira <thyezoliveira@gmail.com>
2021-03-08 21:47:54 -0300

    Crud Js
    Commit unico, fiz um commit só no final do curso.

---
+++ CrudAsyncJs/service/cliente-service.js
@@ -0,0 +1,75 @@
+const listaClientes = () => {
+    return fetch(`http://localhost:3000/profile`)
+    .then( resposta => {
+        if(resposta.ok){
+            return resposta.json();
+        }
+        throw new Error('Não foi possivel listar os clientes!');
+    })
+}
+
+const criaCliente = (nome, email) => {
+    return fetch(`http://localhost:3000/profile`, {
+        method: 'POST',
+        headers: {
+            'Content-Type' : 'application/json'
+        },
+        body: JSON.stringify({
+            nome: nome,
+            email: email
+        })
+    })
+    .then( resposta => {
+        if(resposta.ok){
+            return resposta.body
+        }
+        throw new Error('Não foi possivel criar cliente!');
+    })
+}
+
+const removeCliente = (id) => {
+    return fetch(`http://localhost:3000/profile/${id}`, {
+        method: 'DELETE'
+    }).then( resposta => {
+        if(!resposta.ok){
+            throw new Error('Não foi possivel remover o cliente!');
+        }
+    })
+}
+
+const detalhaCliente = (id) => {
+    return fetch(`http://localhost:3000/profile/${id}`)
+    .then( resposta => {
+        if(resposta.ok){
+            return resposta.json();
+        }
+        throw new Error('Não foi possivel detalhar o cliente!');
+    })
+}
+
+const atualizaCliente = (id, nome, email) => {
+    return fetch(`http://localhost:3000/profile/${id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify({
+            nome: nome,
+            email: email
+        })
+    })
+    .then( resposta => {
+        if(resposta.ok){
+            return resposta.json();
+        }
+        throw new Error('Não foi possivel atualizar cliente!');
+    })
+}
+
+export const clienteService = {
+    listaClientes,
+    criaCliente,
+    removeCliente,
+    detalhaCliente,
+    atualizaCliente
+}
